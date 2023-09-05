e84bb0a6f9fc0fb16ce9f820c8c5101abf8bd43f
Romain Wirth <wirth.romain@gmail.com>
2023-09-05 16:03:39 +0200

    initial commit

---
+++ src/main/java/com/vehicules/microservicevehicules/MicroservicevehiculesApplication.java
@@ -0,0 +1,13 @@
+package com.vehicules.microservicevehicules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicevehiculesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicevehiculesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehicules/microservicevehicules/MicroservicevehiculesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicules.microservicevehicules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicevehiculesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
