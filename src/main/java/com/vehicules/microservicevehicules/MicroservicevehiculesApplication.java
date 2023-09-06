5bc841c024c74de8aa7c52ea4f2dd538fc9882f7
Romain Wirth <wirth.romain@gmail.com>
2023-09-06 11:32:49 +0200

    ADD Swagger, UPD controller, ADD BDD

--- src/main/java/com/vehicules/microservicevehicules/MicroservicevehiculesApplication.java
+++ src/main/java/com/vehicules/microservicevehicules/MicroservicevehiculesApplication.java
@@ -2,8 +2,10 @@ package com.vehicules.microservicevehicules;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MicroservicevehiculesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/vehicules/microservicevehicules/web/controller/VehiculesController.java
+++ src/main/java/com/vehicules/microservicevehicules/web/controller/VehiculesController.java
@@ -2,39 +2,56 @@ package com.vehicules.microservicevehicules.web.controller;
 
 import com.vehicules.microservicevehicules.model.Vehicule;
 import com.vehicules.microservicevehicules.repository.VehiculeRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/vehicules")
 
+@Api("API pour les opérations CRUD sur les véhicules")
 @RestController
 public class VehiculesController {
     @Autowired
     private VehiculeRepository vehiculeRepository;
 
+    @ApiOperation("Méthode pour récupérer tous les véhicules")
     @GetMapping
     public ArrayList<Vehicule> getAllVehicules() {
         return vehiculeRepository.findAll();
     }
 
+    @ApiOperation("Méthode pour récupérer un seul véhicule selon son id")
     @GetMapping("/{id}")
     public @ResponseBody Vehicule displayOneVehicule(@PathVariable int id) {
         return vehiculeRepository.findById(id);
     }
 
+    @ApiOperation("Méthode pour ajouter un nouveau véhicule")
     @PostMapping
     public @ResponseBody Vehicule addNewVehicule (@RequestBody Vehicule newVehicule) {
         return vehiculeRepository.save(newVehicule);
     }
 
+    @PostMapping("/all")
+    public @ResponseBody List<Vehicule> addMultipleVehicules (@RequestBody List<Vehicule> vehicules) {
+        for (Vehicule vehicule: vehicules) {
+            vehiculeRepository.save(vehicule);
+        }
+        return vehicules;
+    }
+
+    @ApiOperation("Méthode pour modifier un véhicule")
     @PutMapping("/{id}")
     public @ResponseBody Vehicule updateVehicule(@PathVariable int id, @RequestBody Vehicule vehicule) {
         vehicule.setId(id);
         return vehiculeRepository.save(vehicule);
     }
 
+    @ApiOperation("Méthode pour supprimer un véhicule de la DB")
     @DeleteMapping("/id")
     public Vehicule deleteVehicule(@PathVariable int id) {
         return vehiculeRepository.deleteById(id);
